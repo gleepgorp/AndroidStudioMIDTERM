4d323e9476f3b4fd8eaeebfde5ae9352031fe7d8
Earl <earlpaquibut@gmail.com>
2023-03-11 20:40:39 +0800

    Calculator Layout + empty methods Vince Tapdasan 3/13/23

--- app/src/main/java/com/example/turonloversmidterm/Calculator.java
+++ app/src/main/java/com/example/turonloversmidterm/Calculator.java
@@ -3,6 +3,7 @@ package com.example.turonloversmidterm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class Calculator extends AppCompatActivity {
 
@@ -10,5 +11,59 @@ public class Calculator extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
+    }
+
+    public void clearOnClick(View view) {
+    }
+
+    public void parenthesesOnClick(View view) {
+    }
+
+    public void circumflexOnClick(View view) {
+    }
+
+    public void sevenOnClick(View view) {
+    }
+
+    public void eightOnClick(View view) {
+    }
+
+    public void nineOnClick(View view) {
+    }
+
+    public void multiplyOnClick(View view) {
+    }
+
+    public void fourOnClick(View view) {
+    }
+
+    public void fiveOnClick(View view) {
+    }
+
+    public void sixOnClick(View view) {
+    }
+
+    public void subtractOnClick(View view) {
+    }
+
+    public void oneOnClick(View view) {
+    }
+
+    public void twoOnClick(View view) {
+    }
+
+    public void threeOnClick(View view) {
+    }
+
+    public void additionOnClick(View view) {
+    }
+
+    public void pointOnClick(View view) {
+    }
+
+    public void zeroOnClick(View view) {
+    }
+
+    public void equalsOnClick(View view) {
     }
 }
